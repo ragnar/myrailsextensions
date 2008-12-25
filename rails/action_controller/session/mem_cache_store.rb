eeecf01a3245551c96477194d044b9bdb29a337f
Ragnar Henriksen <ragnar@shortcut.no>
2008-12-25 14:01:45 +0100

    Fixed regexp for memcache session_id

--- rails/action_controller/session/mem_cache_store.rb
+++ rails/action_controller/session/mem_cache_store.rb
@@ -5,7 +5,7 @@
 class Session
         class MemCacheStore
                 def check_id(id) #:nodoc:#
-                        /[^0-9a-zA-Z-\._]+/ =~ id.to_s ? false : true
+                        /[^0-9a-zA-Z\-\._]+/ =~ id.to_s ? false : true
                 end
         end
 end
