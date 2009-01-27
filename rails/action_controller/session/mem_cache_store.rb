19e3b736da83783d0fa0798acaffb04ae8b77775
Ragnar Henriksen <ragnar@shortcut.no>
2009-01-27 09:59:43 +0100

    Allow % in memcached session_id

--- rails/action_controller/session/mem_cache_store.rb
+++ rails/action_controller/session/mem_cache_store.rb
@@ -6,7 +6,7 @@ class CGI
         class Session
                 class MemCacheStore
                         def check_id(id) #:nodoc:#
-                                /[^0-9a-zA-Z\-\._]+/ =~ id.to_s ? false : true
+                                /[^0-9a-zA-Z\-\._%]+/ =~ id.to_s ? false : true
                         end
                 end
         end
